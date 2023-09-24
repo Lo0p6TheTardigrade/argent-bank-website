3b2e8d1fb336353358b80280d2552cb6407d9177
Lo0p6TheTardigrade <rezonance30@gmail.com>
2023-09-24 04:22:04 +0200

    Home.js page

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import '../scss/pages/Home.scss';
+import Layout from '../components/Layout';
 
 const Home = () => {
-  return <div className="home-container"></div>;
+  return (
+    <div className="home-container">
+      <Layout></Layout>
+    </div>
+  );
 };
 
 export default Home;
