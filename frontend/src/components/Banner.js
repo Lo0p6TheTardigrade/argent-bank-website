774e77d4a8729f2d07f11499ab33e661bf1db3e5
Lo0p6TheTardigrade <rezonance30@gmail.com>
2023-09-24 22:12:59 +0200

    add text to Banner.js

--- frontend/src/components/Banner.js
+++ frontend/src/components/Banner.js
@@ -4,7 +4,15 @@ import '../scss/components/Banner.scss';
 const Banner = () => {
   return (
     <div className="banner-container">
-      <div className="banner"></div>
+      <div className="banner">
+        <section className="card-banner-text">
+          <h2 class="sr-only">Promoted Content</h2>
+          <p class="subtitle">No fees.</p>
+          <p class="subtitle">No minimum deposit.</p>
+          <p class="subtitle">High interest rates.</p>
+          <p class="text">Open a savings account with Argent Bank today!</p>
+        </section>
+      </div>
     </div>
   );
 };
