584c1393056920e9ab1563993f6b526826619d4a
Lo0p6TheTardigrade <rezonance30@gmail.com>
2023-09-24 04:19:35 +0200

    Header.js component

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import '../scss/components/Header.scss';
+import Logo from './Logo';
+import Navigation from './Navigation';
 
 const Header = () => {
   return (
     <div className="header-container">
-      <header className="header"></header>
+      <header className="header">
+        <Logo />
+        <Navigation />
+      </header>
     </div>
   );
 };
