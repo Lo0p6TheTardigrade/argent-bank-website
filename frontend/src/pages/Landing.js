0ca7a0f5788ab7ff6400c489a302eec5d7306c97
Lo0p6TheTardigrade <rezonance30@gmail.com>
2023-09-24 04:23:19 +0200

    Landing.js page

--- frontend/src/pages/Landing.js
+++ frontend/src/pages/Landing.js
@@ -1,8 +1,14 @@
 import React from 'react';
 import '../scss/pages/Landing.scss';
+import Layout from '../components/Layout';
 
 const Landing = () => {
-  return <div className="landing-container"></div>;
+  return (
+    <div className="landing-container">
+      {' '}
+      <Layout></Layout>
+    </div>
+  );
 };
 
 export default Landing;
