3e9e4ad0c7d1d37a2e3e1313377c7baee5f941eb
Lo0p6TheTardigrade <rezonance30@gmail.com>
2023-09-24 16:54:22 +0200

    add logo picture in Logo.js component

--- frontend/src/components/Logo.js
+++ frontend/src/components/Logo.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import '../scss/components/Logo.scss';
+import logo from '../assets/argentBankLogo.png';
 
 const Logo = () => {
   return (
     <div className="logo-container">
       <img
-        src=""
-        alt=""
+        src={logo}
+        alt="logo du site"
         className="logo"
       />
     </div>
