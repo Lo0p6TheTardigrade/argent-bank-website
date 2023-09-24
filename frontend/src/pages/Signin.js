757073bf6522de942a82278d9b1b720bca02d416
Lo0p6TheTardigrade <rezonance30@gmail.com>
2023-09-24 04:24:29 +0200

    Signin.js page

--- frontend/src/pages/Signin.js
+++ frontend/src/pages/Signin.js
@@ -1,8 +1,13 @@
 import React from 'react';
 import '../scss/pages/Signin.scss';
+import Layout from '../components/Layout';
 
 const Signin = () => {
-  return <div className="signin-container"></div>;
+  return (
+    <div className="signin-container">
+      <Layout></Layout>
+    </div>
+  );
 };
 
 export default Signin;
