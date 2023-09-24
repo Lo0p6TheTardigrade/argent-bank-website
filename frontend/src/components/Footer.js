16b7276ff57776dd9f3d2232267cb5e65e8de142
Lo0p6TheTardigrade <rezonance30@gmail.com>
2023-09-24 20:47:33 +0200

    add text to Footer.js + scss

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -4,7 +4,9 @@ import '../scss/components/Footer.scss';
 const Footer = () => {
   return (
     <div className="footer-container">
-      <footer className="footer"></footer>
+      <footer className="footer">
+        <p className="footer-text">Copyright 2020 Argent Bank</p>
+      </footer>
     </div>
   );
 };
