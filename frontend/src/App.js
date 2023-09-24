de4dffa494236499d34f3752fe854d74276b34f8
Lo0p6TheTardigrade <rezonance30@gmail.com>
2023-09-24 17:05:09 +0200

    add route to App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,29 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Signin from './pages/Signin';
+import Landing from './pages/Landing';
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <BrowserRouter>
+        <Routes>
+          <Route
+            path="/home"
+            element={<Home />}
+          />
+          <Route
+            path="/signin"
+            element={<Signin />}
+          />
+          <Route
+            path="landing"
+            element={<Landing />}
+          />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
