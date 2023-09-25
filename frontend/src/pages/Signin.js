062c23fb1f9b7f9da6ca3e204498bf4c06c4ee9a
Lo0p6TheTardigrade <rezonance30@gmail.com>
2023-09-25 19:32:45 +0200

    add SignForm.js component to Signin.js page

--- frontend/src/pages/Signin.js
+++ frontend/src/pages/Signin.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import '../scss/pages/Signin.scss';
 import Layout from '../components/Layout';
+import SignForm from '../components/SignForm';
 
 const Signin = () => {
   return (
     <div className="signin-container">
-      <Layout></Layout>
+      <Layout>
+        <SignForm />
+      </Layout>
     </div>
   );
 };
