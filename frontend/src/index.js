f8e0c71914c6c933ff7f560184907c67fef80573
Lo0p6TheTardigrade <rezonance30@gmail.com>
2023-09-25 17:53:59 +0200

    config Redux in index.js(Top App.js)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,11 +3,21 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import './scss/index.scss';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit';
+import rootReducer from './reducers';
 
+const store = configureStore({
+  reducer: rootReducer,
+  devTools: true,
+});
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
+    ,
   </React.StrictMode>
 );
 
