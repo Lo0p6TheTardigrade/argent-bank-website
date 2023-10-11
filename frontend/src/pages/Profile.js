934b6ae6f6a18c7aedf8538dd524fa1eee800028
Lo0p6TheTardigrade <rezonance30@gmail.com>
2023-10-11 04:49:00 +0200

    Add verification if user is logged to Profile.js page

--- frontend/src/pages/Profile.js
+++ frontend/src/pages/Profile.js
@@ -2,13 +2,20 @@ import React from 'react';
 import '../scss/pages/Profile.scss';
 import UserProfile from '../components/UserProfile';
 import Layout from '../components/Layout';
+import { useSelector } from 'react-redux';
+import Error from '../components/Error';
 
 const Profile = () => {
+  const isLoggedIn = useSelector((state) => state.userReducer.isLoggedIn);
   return (
     <Layout>
-      <div className="profile-container">
-        <UserProfile />
-      </div>
+      {isLoggedIn ? (
+        <div className="profile-container">
+          <UserProfile />
+        </div>
+      ) : (
+        <Error />
+      )}
     </Layout>
   );
 };
