167f8040b48ee25443442d79699c607621e008ca
Lo0p6TheTardigrade <rezonance30@gmail.com>
2023-10-12 01:50:00 +0200

    Add content to Transaction.js +  CLEAN UNNEEDED CODE in Account.scs and User.scss

--- frontend/src/components/Transactions.js
+++ frontend/src/components/Transactions.js
@@ -1,43 +1,60 @@
 import React from 'react';
 import Modal from 'react-modal';
+import '../scss/components/Transactions.scss';
 import Operation from './Operation';
+import { setIsOpen } from '../actions/action';
+import { useDispatch } from 'react-redux';
+import Layout from './Layout';
 
 const Transactions = ({ isOpen }) => {
+  const dispatch = useDispatch();
+  const handleButtonClick = () => {
+    dispatch(setIsOpen(!isOpen));
+  };
   return (
-    <div className="transaction-modal-container">
-      <Modal
-        className="transaction-modal"
-        isOpen={isOpen}>
-        <div className="transaction-modal-top-info-container">
-          <div className="transaction-modal-info-cantainer">
-            <h3 className="transaction-checking-title">Argent Bank Checking (x3448)</h3>
-            <p className="transaction-balance">$48,098.43</p>
-            <p className="tarsaction-balance-info">Availiable balance</p>
+    <Modal
+      className="transaction-modal"
+      isOpen={isOpen}>
+      <Layout>
+        <div className="transaction-modal-container">
+          <div className="top-box">
+            <div className="transaction-modal-top-info-container">
+              <div className="transaction-modal-info-cantainer">
+                <h3 className="transaction-checking-title">Argent Bank Checking (x3448)</h3>
+                <p className="transaction-balance">$48,098.43</p>
+                <p className="transaction-balance-info">Availiable balance</p>
+              </div>
+            </div>
+            <div className="transaction-balance-cross-container">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                height="80px"
+                width="80px"
+                viewBox="0 0 384 512"
+                fill="white"
+                className="transaction-balance-cross"
+                onClick={handleButtonClick}>
+                <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+              </svg>
+            </div>
+          </div>
+          <div className="transaction-modal-bottom-container">
+            <div className="transaction-modal-operation-title">
+              <span className="transaction-by-date">Date</span>
+              <span className="transaction-by-description">Description</span>
+              <span className="transaction-by-amount">Amount</span>
+              <span className="transaction-by-balance">Balance</span>
+            </div>
+            <div className="transaction-operation">
+              <Operation />
+              <Operation />
+              <Operation />
+              <Operation />
+            </div>
           </div>
         </div>
-        <div className="transaction-balance-cross-container">
-          <img
-            src="cross close"
-            alt="cross logo"
-            className="transaction-balance-cross"
-          />
-        </div>
-        <div className="transaction-modal-bottom-container">
-          <div className="transaction-modal-operation-title">
-            <p className="transaction-by-date">Date</p>
-            <p className="transaction-by-description">Description</p>
-            <p className="transaction-by-amount">Aùount</p>
-            <p className="transaction-by-balance">Balance</p>
-          </div>
-          <div className="transaction-operation">
-            <Operation />
-            <Operation />
-            <Operation />
-            <Operation />
-          </div>
-        </div>
-      </Modal>
-    </div>
+      </Layout>
+    </Modal>
   );
 };
 
